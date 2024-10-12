4e9e045ee223129127e7bddd7611191fcf71add8
Peter <liuqi20328@gmail.com>
2024-10-12 19:43:51 +0800

    [feature] change software version
    1. change software version to 0.1.0
    
    Signed-off-by: Peter <liuqi20328@gmail.com>

--- app/common/setting.py
+++ app/common/setting.py
@@ -7,7 +7,7 @@ DEBUG = "__compiled__" not in globals()
 
 YEAR = 2024
 AUTHOR = "iliuqi"
-VERSION = "v0.0.3"
+VERSION = "v0.1.0"
 APP_NAME = "OneMore"
 HELP_URL = "https://github.com/miniLQ/onemore/issues"
 REPO_URL = "https://github.com/miniLQ/onemore"
