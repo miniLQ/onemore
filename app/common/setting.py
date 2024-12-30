d50c63ae680e010d80da0484cbe8de6f5571cf08
miniLQ <liuqi20328@gmail.com>
2024-12-30 17:41:01 +0800

    Release v0.2.1

--- app/common/setting.py
+++ app/common/setting.py
@@ -7,7 +7,7 @@ DEBUG = "__compiled__" not in globals()
 
 YEAR = 2024
 AUTHOR = "iliuqi"
-VERSION = "v0.2.0"
+VERSION = "v0.2.1"
 APP_NAME = "OneMore"
 HELP_URL = "https://github.com/miniLQ/onemore/issues"
 REPO_URL = "https://github.com/miniLQ/onemore"
