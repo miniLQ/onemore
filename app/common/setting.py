2075fa3fc734b3c3d36c0e719d5d8b4f16ae70e2
Peter <liuqi20328@gmail.com>
2025-03-13 20:03:18 +0800

    Release v1.0.0
    1. 更新gdb工具到14.x
    2. 更新objdump为llvm-objdump
    3. 增加platform_config的解析功能
    4. 更新nm工具为llvm-nm
    5. 支持linux ramdump parser工具的单组件解析
    
    Signed-off-by: Peter <liuqi20328@gmail.com>

--- app/common/setting.py
+++ app/common/setting.py
@@ -7,7 +7,7 @@ DEBUG = "__compiled__" not in globals()
 
 YEAR = 2024
 AUTHOR = "iliuqi"
-VERSION = "v0.4.0"
+VERSION = "v1.0.0"
 APP_NAME = "OneMore"
 HELP_URL = "https://github.com/miniLQ/onemore/issues"
 REPO_URL = "https://github.com/miniLQ/onemore"
