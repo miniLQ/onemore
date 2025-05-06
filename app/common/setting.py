938636c0e3f0310c134cf331b59f55f6dd22f806
Peter <liuqi20328@gmail.com>
2025-05-06 11:51:00 +0800

    Release v1.1.0
    1. 更新linux ramdump parser工具版本到opensource-tools.lnx.1.0.r297
    2. 增加tz log decode工具
    3. 修复一些bug
    
    Signed-off-by: Peter <liuqi20328@gmail.com>

--- app/common/setting.py
+++ app/common/setting.py
@@ -7,7 +7,7 @@ DEBUG = "__compiled__" not in globals()
 
 YEAR = 2024
 AUTHOR = "iliuqi"
-VERSION = "v1.0.1"
+VERSION = "v1.1.0"
 APP_NAME = "OneMore"
 HELP_URL = "https://github.com/miniLQ/onemore/issues"
 REPO_URL = "https://github.com/miniLQ/onemore"
